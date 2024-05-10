50fdac57150ea4485f47e15675e43a699eefe770
JoseRGM9 <157386833+JoseRGM9@users.noreply.github.com>
2024-05-10 15:36:38 +0000

    commit10

--- src/models.py
+++ src/models.py
@@ -11,8 +11,8 @@ class People(Base):
     __tablename__ = "people"
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    height = Column(String(250))
-    mass = Column(String(250))
+    height = Column(Integer)
+    mass = Column(Integer)
     gender = Column(String(250))
 
 class Planet(Base):
