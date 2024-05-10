2256ab0c92c6e56f39bc7503132b15dcac31b43b
JoseRGM9 <157386833+JoseRGM9@users.noreply.github.com>
2024-05-10 15:27:51 +0000

    commit9

--- src/models.py
+++ src/models.py
@@ -7,23 +7,40 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class People(Base):
+    __tablename__ = "people"
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    height = Column(String(250))
+    mass = Column(String(250))
+    gender = Column(String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planet(Base):
+    __tablename__ = "planet"
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    climate = Column(String(250))
+    terrain = Column(String(250))
+
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    people_id = Column(Integer, ForeignKey('people.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    usuario_id = Column(Integer, ForeignKey('usuario.id'))
+    people = relationship("People", backref="favorites")
+    planet = relationship("Planet", backref="favorites")
+    usuario = relationship("Usuario", backref="favorites")
+
+class Usuario(Base):
+    __tablename__ = 'usuario'
+    id = Column(Integer, primary_key=True)
+    nombre_usuario = Column(String(250), nullable=False, unique=True)
+    nombre = Column(String(250), nullable=False)
+    contraseña = Column(String(250), nullable=False)
+    correo_electronico = Column(String(250), nullable=False, unique=True)
+    favorites = relationship("Favorites", backref="usuario")
 
     def to_dict(self):
         return {}
